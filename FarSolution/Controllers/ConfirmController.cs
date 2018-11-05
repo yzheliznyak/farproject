7287172fd08d00d535f6edf2b7e7b3e65e80d23c
Yaro <qwest12ya@gmail.com>
2018-11-05 14:22:54 +0200

    Additional functionality.

---
+++ FarSolution/Controllers/ConfirmController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarSolution.Controllers
+{
+    public class ConfirmController : Controller
+    {
+        public bool Index()
+        {
+            return true;
+        }
+    }
+}
--- FarSolution/wwwroot/js/site.js
+++ FarSolution/wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿// Write your JavaScript code.
+
+$(document).ready(function() {
+    var count = 0;
+    $('.bindEvent').click(function(data) {
+        changeEvent(data);
+    });
+
+    function changeEvent(data) {
+        /*console.log(data); */
+        count ++;
+        document.getElementById("count").innerHTML =  'Confirm(' + count + ')';
+    }
+
+});
